188c5e7dbecd9bd98085973c62f1a88d9dca943b
SummerEllada <summer.ellada@gmail.com>
2022-04-06 18:01:10 -0400

    first commit

---
+++ PytonSyntax.py
@@ -0,0 +1,6 @@
+title = "Python in easy steps"
+print( title )
+num = 3
+print (num * 8 +4 )
+print (num * (8 +4) )
+
---
+++ ScriptPractice.py
@@ -0,0 +1,5 @@
+user = input( "I'm Python. What is your name? : " )
+print( "Welcome", user )
+lang = input(" Favorite programming language? : " )
+print( lang, "Is" , "Fun"  , sep = "*" , end = "!\n" )
+
